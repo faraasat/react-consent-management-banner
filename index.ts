48b402c15a5e077e058daa762e699549de3c80bd
Farasat Ali <63093876+faraasat@users.noreply.github.com>
2025-06-02 18:49:52 +0500

    fix: import issue

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { default as CookieConsent } from "./src/CookieConsent";
-export type {
-  IPreferenceOption,
-  IMoreLinks,
-  CookieConsentConfig,
-  Props,
-} from "./src/types";
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as CookieConsent } from "./consent-management";
+export type {
+  IPreferenceOption,
+  IMoreLinks,
+  CookieConsentConfig,
+  Props,
+} from "./types";
