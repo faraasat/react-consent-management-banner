4aba39e4ea5effa7283e1869e299ea426e020dc3
Farasat Ali <63093876+faraasat@users.noreply.github.com>
2025-07-20 01:41:27 +0500

    chore: fixed package size

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   entry: ["src/index.tsx"],
   format: ["cjs", "esm"],
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   splitting: false,
   target: "es2017",
